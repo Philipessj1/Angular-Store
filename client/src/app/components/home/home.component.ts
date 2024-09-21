7090d4cd46afee2dda9b7a29b72d432fbef055a8
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-09-21 13:02:29 -0300

    feat: product component created

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
+import { ProductComponent } from '../product/product.component';
+import { Product } from '../../../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products) => {
-        console.log(products);
+        this.products = products.items;
       });
   }
 }
