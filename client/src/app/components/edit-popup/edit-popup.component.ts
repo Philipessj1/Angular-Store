902db411a8d76a6c08c7fe253dad038cff5fb0f5
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-10-10 16:29:42 -0300

    feat: CRUD UI and Functionality added

---
+++ client/src/app/components/edit-popup/edit-popup.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { Product } from '../../../types';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'primeng/rating';
+
+@Component({
+  selector: 'app-edit-popup',
+  standalone: true,
+  imports: [DialogModule, CommonModule, ButtonModule, FormsModule, RatingModule],
+  templateUrl: './edit-popup.component.html',
+  styleUrl: './edit-popup.component.scss'
+})
+export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Input() header!: string;
+  @Input() product: Product = {
+    name: '',
+    image: '',
+    price: '',
+    rating: 0
+  };
+  @Output() confirm = new EventEmitter<Product>();
+  @Output() cancel = new EventEmitter<void>();
+  @Output() displayChange = new EventEmitter<boolean>();
+
+  onConfirm() {
+    this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+
+  onCancel() {
+    this.display = false;
+    this.displayChange.emit(this.display);
+  }
+}
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -4,12 +4,14 @@ import { ProductComponent } from '../product/product.component';
 import { Product, Products } from '../../../types';
 import { CommonModule } from '@angular/common';
 import { PaginatorModule } from 'primeng/paginator';
+import { ButtonModule } from 'primeng/button';
 import { environment } from '../../../environments/environment.development';
+import { EditPopupComponent } from "../edit-popup/edit-popup.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule, PaginatorModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule, EditPopupComponent, ButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -20,6 +22,45 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  /* ADD/EDIT Popup */
+  selectedProduct: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    price: '',
+    rating: 0
+  }
+
+  displayAddProduct: boolean = false;
+  displayEditProduct: boolean = false;
+
+  toggleAddPopup() {
+    this.displayAddProduct = true;
+  }
+
+  toggleEditPopup(product: Product) {
+    this.selectedProduct = product;
+    this.displayEditProduct = true;
+  }
+
+  toggleDeletePopup(product: Product) {
+    if (!product.id) return;
+    this.deleteProduct(product.id);
+  }
+  
+  onConfirmAdd(product: Product) {
+    this.addProduct(product);
+    this.displayAddProduct = false;
+  }
+
+  onConfirmEdit(product: Product) {
+    if (!this.selectedProduct.id) return;
+
+    this.editProduct(product, this.selectedProduct.id);
+    this.displayAddProduct = false;
+  }
+
+  /* Paginator */
   totalRecords: number = 0;
   rows: number = 5;
 
@@ -27,6 +68,7 @@ export class HomeComponent {
     this.fetchProducts(event.page, event.rows);
   }
 
+  /* GET Product */
   fetchProducts(page:number, perPage:number) {
     this.productsService
       .getProducts(`${this.apiUrl}/clothes`, { page, perPage })
@@ -39,18 +81,7 @@ export class HomeComponent {
       })
   }
 
-  editProduct(product: Product, id: number) {
-    this.productsService.editProduct(`${this.apiUrl}/clothes/${id}`, product).subscribe(
-      {
-        next: data => {
-          console.log(data)
-          this.fetchProducts(0,this.rows);
-        },
-        error: error => console.log(error)
-      }
-    )
-  }
-
+  /* CREATE Product */
   addProduct(product: Product) {
     this.productsService.addProduct(`${this.apiUrl}/clothes/`, product).subscribe(
       {
@@ -63,6 +94,20 @@ export class HomeComponent {
     )
   }
 
+  /* PUT Product */
+  editProduct(product: Product, id: number) {
+    this.productsService.editProduct(`${this.apiUrl}/clothes/${id}`, product).subscribe(
+      {
+        next: data => {
+          console.log(data)
+          this.fetchProducts(0,this.rows);
+        },
+        error: error => console.log(error)
+      }
+    )
+  }
+
+  /* DELETE Product */
   deleteProduct(id: number) {
     this.productsService.deleteProduct(`${this.apiUrl}/clothes/${id}`).subscribe(
       {
