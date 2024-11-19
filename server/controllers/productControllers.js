7208f21ce8acb7277c851e3ce6b36b37aa11f5f2
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2024-11-19 16:18:22 -0300

    feat: All CRUD routes created

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -1,7 +1,127 @@
 import { Product as ProductModel } from "../models/Product.js";
 
 const productController = {
+  //CREATE
+  create: async (req, res) => {
+    try {
+      //Product Object
+      const product = {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        rating: req.body.rating,
+      };
 
-}
+      //Post product on DB
+      const response = await ProductModel.create(product);
+
+      //Return product posted with a successfull message
+      res.status(201).json({ response, msg: "Product created successfully!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //GETALL
+  getall: async (req, res) => {
+    //get page and perPage from req.query
+    const page = parseInt(req.query.page) || 0;
+    const perPage = parseInt(req.query.perPage) || 10;
+
+    const start = page * perPage;
+    const end = start + perPage;
+
+    try {
+      //get products from DB
+      const products = await ProductModel.find();
+
+      //slice products to the amount requested
+      const response = products.slice(start, end);
+
+      //return products
+      res.json({
+        items: response,
+        total: products.length,
+        page,
+        perPage,
+        totalPages: Math.ceil(products.length / perPage),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //GET
+  get: async (req, res) => {
+    try {
+      //get id from req.params
+      const id = req.params.id;
+
+      //get the requested product by id on DB
+      const product = await ProductModel.findById(id);
+
+      //check if product exists
+      if (!product) {
+        res.status(404).json({ msg: "Product not Found" });
+        return;
+      }
+
+      res.json(product);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      //get id from req.params
+      const id = req.params.id;
+
+      //delete the requested product by id on DB
+      const deletedProduct = await ProductModel.findByIdAndDelete(id);
+
+      //check if products exists
+      if (!deletedProduct) {
+        res.status(404).json({ msg: "Product not Found" });
+        return;
+      }
+
+      //return the deleted product with a successfull message
+      res
+        .status(200)
+        .json({ deletedProduct, msg: "Product deleted successfully" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //UPDATE
+  update: async (req, res) => {
+    try {
+      //get id from req.params
+      const id = req.params.id;
+
+      //Product Object
+      const product = {
+        name: req.body.name,
+        price: req.body.price,
+        image: req.body.image,
+        rating: req.body.rating,
+      };
+
+      //Update the selected product on DB
+      const updatedProduct = await ProductModel.findByIdAndUpdate(id, product);
+
+      //check if products exists
+      if (!updatedProduct) {
+        res.status(404).json({ msg: "Product not Found" });
+        return;
+      }
+
+      //return the updated product with a successfull message
+      res
+        .status(200)
+        .json({ updatedProduct, msg: "Product updated successfully" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 
 export { productController };
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,37 @@
-import express from 'express';
+import express from "express";
 
-import { productController } from '../controllers/productControllers.js';
+import { productController } from "../controllers/productControllers.js";
 
 const productsRouter = express.Router();
+
+//POST route
+//example: http://localhost:8800/api/products
+productsRouter
+  .route("/")
+  .post((req, res) => productController.create(req, res));
+
+// GET(All) route
+// example: http://localhost:8800/api/products?page=0&perPage=2
+productsRouter
+  .route('/')
+  .get((req, res) => productController.getall(req, res));
+
+// GET(One) route
+// example: http://localhost:8800/api/products/:id
+productsRouter
+  .route('/:id')
+  .get((req, res) => productController.get(req, res));
+
+// DELETE route
+// example: http://localhost:8800/api/products/:id
+productsRouter
+  .route('/:id')
+  .delete((req, res) => productController.delete(req, res));
+
+//PUT route
+//example: http://localhost:8800/api/products/:id
+productsRouter
+  .route("/:id")
+  .put((req, res) => productController.update(req, res));
 
 export default productsRouter;
